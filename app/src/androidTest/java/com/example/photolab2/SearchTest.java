fda45a0f3e3ed3f0f9c9cc499ee767d2b08218c9
renzop5231 <renzop5231@gmail.com>
2020-10-22 22:50:16 -0700

    changing gradle settings

--- app/src/androidTest/java/com/example/photolab2/SearchTest.java
+++ app/src/androidTest/java/com/example/photolab2/SearchTest.java
@@ -1,27 +1,25 @@
 package com.example.photolab2;
 
-import android.app.Activity;
 import android.content.Context;
 
-import androidx.test.espresso.ViewAction;
 import androidx.test.espresso.action.ViewActions;
-import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.rule.ActivityTestRule;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import static org.junit.Assert.*;
 
-// Espresso imports
-import static androidx.test.espresso.Espresso.closeSoftKeyboard;
+import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.junit.Assert.assertEquals;
+
+// Espresso imports
 
 @RunWith(AndroidJUnit4.class)
 public class SearchTest {
